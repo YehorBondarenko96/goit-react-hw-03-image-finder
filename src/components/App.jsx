0e2a2f6cdef368b934ef55deeeb8c7495b6b744b
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-12-18 14:25:48 +0200

    1.1.0-18.12.23

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,39 @@
 import css from './Styles.module.css';
 import { Component } from 'react';
+import fetchForSearch from '../services/api';
 import { Searchbar } from "./Searchbar/Searchbar";
 
 export class App extends Component {
+  state = {
+    results: [],
+    isLoading: false,
+    error: null,
+  };
+
+  intV = '';
+  page = 1;
+
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+
+    try {
+      const results = await fetchForSearch(this.intV, this.page);
+      console.log(1);
+      this.setState({ results });
+    } catch (error) {
+      console.log(2);
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
   inputValue = (evt) => {
     evt.preventDefault();
-    const intV = evt.currentTarget.elements.search.value;
-    console.log(intV);
+    this.intV = evt.currentTarget.elements.search.value;
+    console.log(this.intV);
   }
   render () {
     return (
